3358a78c3132f692097939c9e012d9c0341d83f3
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-11-11 10:50:48 +0100

    Initial Commit

---
+++ Stanford-CS193p-Programming-Assignment-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford-CS193p-Programming-Assignment-2
+//
+//  Created by Zeljko Lucic on 11.11.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford-CS193p-Programming-Assignment-2/Stanford_CS193p_Programming_Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_CS193p_Programming_Assignment_2App.swift
+//  Stanford-CS193p-Programming-Assignment-2
+//
+//  Created by Zeljko Lucic on 11.11.23..
+//
+
+import SwiftUI
+
+@main
+struct Stanford_CS193p_Programming_Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
